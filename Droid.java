592dc24d4be5c5060ede0bd6d27fc9a155a5c64c
unknown <arturodiazmay@gmail.com>
2023-01-20 04:45:36 -0600

    Create Droid.java with a Droid Class + Constructor + Created an Instance of A Droid Object

---
+++ Droid.java
@@ -0,0 +1,14 @@
+public class Droid {
+  String name;
+  int batteryLevel;
+
+  public Droid(String droidName) {
+    name = droidName;
+    batteryLevel = 100;
+  }
+
+  public static void main(String[] args) {
+    Droid codey = new Droid("Codey");
+  }
+
+}
