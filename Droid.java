843f644c94fd26299b193c22c7afa2c32a2a78c5
unknown <arturodiazmay@gmail.com>
2023-01-20 04:56:58 -0600

    Created a performTask Method

--- Droid.java
+++ Droid.java
@@ -1,14 +1,25 @@
 public class Droid {
-  String name;
-  int batteryLevel;
-
-  public Droid(String droidName) {
-    name = droidName;
-    batteryLevel = 100;
+    String name;
+    int batteryLevel;
+  
+    public Droid(String droidName) {
+      name = droidName;
+      batteryLevel = 100;
+    }
+  
+    public String toString() {
+      return "Hello I am the droid " + name + "! Not to be confused with R2D2.";
+    }
+  
+    public void performTask(String task) {
+      System.out.println(name + " is performing task: " + task);
+      batteryLevel -= 10;
+    }
+  
+    public static void main(String[] args) {
+      Droid codey = new Droid("Codey");
+      System.out.println(codey);
+      codey.performTask("Clean my room");
+    }
+  
   }
-
-  public static void main(String[] args) {
-    Droid codey = new Droid("Codey");
-  }
-
-}
