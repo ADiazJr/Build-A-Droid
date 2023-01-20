3d6476352642847658e91608ed2a929fa5b5b74b
unknown <arturodiazmay@gmail.com>
2023-01-20 05:13:28 -0600

    Created energyReport Method + Created energyTransfer Method Which Allows Instances to Transfer batteryLevel

--- Droid.java
+++ Droid.java
@@ -10,7 +10,17 @@ public class Droid {
     public String toString() {
       return "Hello I am the droid " + name + "! Not to be confused with R2D2.";
     }
-  
+    
+    public void energyReport() {
+        System.out.println(name + " is currently at " + batteryLevel + "% battery!");
+    }  
+
+    public void energyTransfer(int amountToTransfer, Droid transferBuddy) {
+        transferBuddy.batteryLevel -= amountToTransfer;
+        batteryLevel += amountToTransfer;
+        System.out.println("My battery is now at " + batteryLevel + "% and " + transferBuddy.name + " is now at " + transferBuddy.batteryLevel + "%. I gained " + amountToTransfer + "% battery!");
+    }
+
     public void performTask(String task) {
       System.out.println(name + " is performing task: " + task);
       batteryLevel -= 10;
@@ -20,6 +30,9 @@ public class Droid {
       Droid codey = new Droid("Codey");
       System.out.println(codey);
       codey.performTask("Clean my room");
+      codey.energyReport();
+      Droid nodey = new Droid("nodey");
+      codey.energyTransfer(10, nodey);
     }
   
   }
